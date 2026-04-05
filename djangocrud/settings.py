5246aceb630a95164bf32d0bee1254515ecde46f
Brayan Salamanca <salamancaayalabrayandavid@gmail.com>
2026-04-05 05:50:32 +0000

    Fix 502 error: update requirements and clean imports

--- djangocrud/settings.py
+++ djangocrud/settings.py
@@ -125,7 +125,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'saebra581@gmail.com'
-EMAIL_HOST_PASSWORD = 'wvvzqyvfozyegare' 
+EMAIL_HOST_PASSWORD = 'lhuu kgqv mxfo xaju' 
 DEFAULT_FROM_EMAIL = 'Live Fútbol <saebra581@gmail.com>'
 
 # 7. Seguridad CORS y CSRF
