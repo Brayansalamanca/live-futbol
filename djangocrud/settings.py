beec48d3cae315bcb0c2457cfa01673bbc4eebc6
Brayan Salamanca <salamancaayalabrayandavid@gmail.com>
2026-04-07 14:27:27 +0000

    Solución error 403 CSRF y ajuste de correo

--- djangocrud/settings.py
+++ djangocrud/settings.py
@@ -141,6 +141,7 @@ CORS_ALLOWED_ORIGINS = [
 CSRF_TRUSTED_ORIGINS = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
+    "https://django2-xo79.onrender.com",
     "https://*.onrender.com"
 ]
 
